be684bf55c28bab36265e309eb2c6a06aece0799
axe-rigged <alex20101@student.hamk.fi>
2022-08-26 23:56:15 +0300

    Made first model.

--- beertaste/sites/models.py
+++ beertaste/sites/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from stdimage import StdimageField, JPEGField
+from stdimage.validators import MinSizeValidator
 
 # Create your models here.
+class beerReview(models.Model):
+    beer_name = models.CharField(max_lenght=200)
+    starts = models.IntegerField()
+    image = JPEGField(upload_to='images/', blank=false, variations={
+    'thumbnail':(400, 400, True)}, delete_orphans=True, validators=[MinSizeValidator(400,400)])
+    drank_date = models.DateTimeField('data published', auto_now_add=True)
+    description = models.CharField(max_lenght=250)
+
+#Maybe small brief for
