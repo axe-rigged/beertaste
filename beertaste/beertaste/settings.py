f244778adef94f0c8da5c889fcb90b7196c4af5d
axe-rigged <alex20101@student.hamk.fi>
2022-08-29 01:40:03 +0300

    Static system works now.

--- beertaste/beertaste/settings.py
+++ beertaste/beertaste/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'beertaste.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "beertaste/templates")],
+        'DIRS': [BASE_DIR / "beertaste/templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,11 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 # /var/www/beer/media+/images, statics
-
-STATICFIELS_DIRS = [os.path.join(BASE_DIR, "beertaste/static")]
-STATIC_URL = 'static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+        BASE_DIR / "beertaste/static/",
+        BASE_DIR / "sites/static/",
+        ]
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+#Tarkista tarvitseeko olla os.path.join vai uudella 4.0 tavalla
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
