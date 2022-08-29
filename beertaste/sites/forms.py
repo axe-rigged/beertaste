7fc0f43c0d173e872d6ffe19c6c4a4a108bf6637
axe-rigged <alex20101@student.hamk.fi>
2022-08-30 00:53:32 +0300

    Small progress on views.py and made forms.

---
+++ beertaste/sites/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import beerReview
+
+class UploadReview(forms.ModelForm):
+    class Meta:
+        model = beerReview
+        fields = ["beer_name", "stars", "image", "description"]
--- beertaste/sites/views.py
+++ beertaste/sites/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import beerReview
+from .forms import UploadReview
 
 def beerIndex(request):
-    return render(request, "sites/index.html", {})
+    if request.method == "POST":
+        #Validate that post is legit and then return index With get.
+       form = UploadReview(request.POST, request.FILES)
+       if form.is_valid:
+           review = form.save(commit=False)
+           #We later need to check start and maybe something else
+           review.save()
+           return redirect("sites:index")
+    else:
+        form = UploadReview()
+    if request.method == "GET":
+        return render(request, "sites/index.html", {"form" : form})
