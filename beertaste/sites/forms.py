ebabefac9122d2694fbec151cc34339a5fe1386e
axe-rigged <alex20101@student.hamk.fi>
2022-09-09 00:36:35 +0300

    Fixed little bit forms page and made small change on look.

--- beertaste/sites/forms.py
+++ beertaste/sites/forms.py
@@ -4,4 +4,4 @@ from .models import beerReview
 class UploadReview(forms.ModelForm):
     class Meta:
         model = beerReview
-        fields = ["beer_name", "stars", "image", "description"]
+        fields = ["beer_name", "stars", "image", "description", "adjectives", "countryMade"]
