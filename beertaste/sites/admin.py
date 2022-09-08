d921d0eb96fe550138d0d783c51395c5eab320c6
axe-rigged <alex20101@student.hamk.fi>
2022-09-08 23:49:05 +0300

    Change made in models.

--- beertaste/sites/admin.py
+++ beertaste/sites/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import beerReview
+from .models import beerReview, tags, country
 
 # Register your models here.
 #Look more ways to make admin-page look better from django doc.
 class beerReviewAdmin(admin.ModelAdmin):
     list_display = ("beer_name", "image", "stars", "drank_date")
     search_fields = ["description"]
+
+class tagsForBeer(admin.ModelAdmin):
+    list_display = ("adjective")
+
+class beerHome(admin.ModelAdmin):
+    list_display = ("country_name")
 
 admin.site.register(beerReview, beerReviewAdmin)
--- beertaste/sites/models.py
+++ beertaste/sites/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from stdimage import StdImageField, JPEGField
 from stdimage.validators import MinSizeValidator
 
+#County where it is made
+class country(models.Model):
+    country_name = models.CharField(max_length=100)
+
+#Tags to be used for beer
+class tags(models.Model):
+    adjective = models.CharField(max_length=100)
+
 # Create your models here.
 class beerReview(models.Model):
     beer_name = models.CharField(max_length=200)
@@ -9,6 +17,7 @@ class beerReview(models.Model):
     image = JPEGField(upload_to='images/', blank=False, variations={
     'thumbnail':(400, 400, True)}, delete_orphans=True, validators=[MinSizeValidator(400,400)])
     drank_date = models.DateTimeField('data published', auto_now_add=True)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=300)
+    adjectives = models.ManyToManyField(tags)
+    countryMade = models.ForeignKey(country, on_delete=models.CASCADE, null=True)
 
-#Maybe small brief for
