04e50f7eae38d874b5f827d4751b67e74293a754
axe-rigged <alex20101@student.hamk.fi>
2022-09-19 20:31:20 +0300

    Fixed models and made links for moving website.

--- beertaste/sites/forms.py
+++ beertaste/sites/forms.py
@@ -4,4 +4,4 @@ from .models import beerReview
 class UploadReview(forms.ModelForm):
     class Meta:
         model = beerReview
-        fields = ["beer_name", "stars", "image", "description", "adjectives", "countryMade"]
+        fields = ["beer_name", "stars", "image", "description", "adjectives", "beer_type","countryMade"]
